ba2be53fe11f1d52e1e749913b7e3fe753a87954
Lorna Kamau <kamaulorna@gmail.com>
2020-06-10 13:48:31 +0300

    create login function

--- app/auth/views.py
+++ app/auth/views.py
@@ -10,7 +10,6 @@ from ..email import mail_message
 @auth.route('/signup', methods = ["GET", "POST"])
 def signup():
     form = SignUpForm()
-    categories = Category.query.all()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
@@ -20,3 +19,16 @@ def signup():
         return redirect(url_for('auth.login'))
     title = "New Account | Pitch"
     return render_template('auth/signup.html', signup_form = form, title=title)
+
+@auth.route('/login', methods = ["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.home'))
+        flash('Invalid username or password', 'danger')
+    
+    title = "Login | Pitch"
+    return render_template('auth/login.html', login_form = form, title=title)
