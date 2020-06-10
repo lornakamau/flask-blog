c69f5151c8e0f27b0b492a7aacbaf7d85ca4042a
Lorna Kamau <kamaulorna@gmail.com>
2020-06-10 14:38:44 +0300

    add new_post function

--- app/main/views.py
+++ app/main/views.py
@@ -9,3 +9,17 @@ def index():
     View root page function that returns the index page and its data
     '''
     return render_template('home.html')
+
+@main.route('/new-pitch', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    title = 'New Pitch | Pitch'
+    form = PitchForm()
+    if form.validate_on_submit():
+        post = Pitch(post_content=form.post_content.data, author=current_user, title=form.title.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been posted!', 'success')
+        return redirect(url_for('main.posts'))
+
+    return render_template('posts/create_post.html',title=title, pitch_form=form)
